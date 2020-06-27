ff014cb40840740f361f1bbd393368bd068cb343
Dan Phifer <dan.phifer@gmail.com>
2020-06-27 14:39:05 -0400

    Updating tests again for readability.

--- test/model-test.js
+++ test/model-test.js
@@ -3,64 +3,63 @@ const model = require('../index');
 
 describe('model', function () {
   it('should parse parameters', function () {
-    expectModel({
-        customer: 'string',
-        points: {type: 'int', default: 10},
-        subscriber: 'bool',
-        percentage: 'float'
-      },
-      {
+    withModel({
+      customer: 'string',
+      points: {type: 'int', default: 10},
+      subscriber: 'bool',
+      percentage: 'float',
+      json: 'object'
+    })
+      .request({
         query: {
           customer: 'dan',
           subscriber: 'true',
           points: 20,
           percentage: 99.3
+        },
+        body: {
+          json: {hello: 'world'}
         }
-      },
-      {
+      })
+      .expect({
         customer: 'dan',
         subscriber: true,
         points: 20,
-        percentage: 99.3
-      }
-    )
-  });
-
-  it('throws on invalid definition', function () {
-    expectError(
-      {customer: 'badType'},
-      {query: {customer: 'something'}})
-  });
-
-  it('throws on missing required params', function () {
-    expectError(
-      {required: 'string'},
-      {query: {x: 1}})
-  });
-
-  it('uses defaults', function () {
-    expectModel({
-      x: {type: 'int', default: 10}
-    },
-      {
-      },
-      {
-        x: 10
+        percentage: 99.3,
+        json: {hello: 'world'}
       })
   });
 
+  it('throws on invalid definition', function () {
+    withModel({customer: 'badType'})
+      .request({query: {customer: 'something'}})
+      .expectError();
+  });
+
+  it('throws on missing required params', function () {
+    withModel({required: 'string'})
+      .request({query: {x: 1}})
+      .expectError();
+  });
+
+  it('uses defaults', function () {
+    withModel({x: {type: 'int', default: 10}})
+      .request({})
+      .expect({x: 10})
+  });
+
   it('reads in priority order', function () {
-    expectModel({
-        x: 'string',
-        y: 'string',
-        z: 'string'
-      },
-      {
+    withModel({
+      x: 'string',
+      y: 'string',
+      z: 'string'
+    })
+      .request({
         params: {x: 'paramsX'},
         query: {x: 'queryX', y: 'queryY'},
         body: {x: 'bodyX', y: 'bodyY', z: 'bodyZ'}
-      },
-      {
+      })
+      .expect({
         x: 'paramsX',
         y: 'queryY',
         z: 'bodyZ'
@@ -68,89 +67,104 @@ describe('model', function () {
   });
 
   it('reads from sources', function () {
-    expectModel({
-        x: {type: 'string', sources: ['body']},
-        y: 'string',
-        z: 'string'
-      },
-      {
-        params: {x: 'paramsX'},
-        query: {x: 'queryX', y: 'queryY'},
-        body: {x: 'bodyX', y: 'bodyY', z: 'bodyZ'}
-      },
-      {
-        x: 'bodyX',
-        y: 'queryY',
-        z: 'bodyZ'
-      });
+    withModel({
+      x: {type: 'string', sources: ['body']},
+      y: 'string',
+      z: 'string'
+    })
+      .request(
+        {
+          params: {x: 'paramsX'},
+          query: {x: 'queryX', y: 'queryY'},
+          body: {x: 'bodyX', y: 'bodyY', z: 'bodyZ'}
+        })
+      .expect(
+        {
+          x: 'bodyX',
+          y: 'queryY',
+          z: 'bodyZ'
+        });
   });
 
 
   it('reads objects', function () {
-    expectModel(
-      { x: {type: 'object'}},
-      {body: {x: {a: 1, b: 2, c: 3}}},
-      {x: {a: 1, b: 2, c: 3}}
-    )
+    withModel({x: {type: 'object'}})
+      .request({
+        body: {
+          x: {a: 1, b: 2, c: 3}
+        }
+      })
+      .expect({
+        x: {a: 1, b: 2, c: 3}
+      })
   });
 
   it('allows valid enum values', function () {
-    expectModel(
-      { food: {type: 'string', enum: ['pizza', 'hamburger', 'steak']}},
-      {body: {food: 'pizza'}},
-      {food: 'pizza'}
-    )
+    withModel({
+        food: {type: 'string', enum: ['pizza', 'hamburger', 'steak']}
+      })
+      .request({body: {food: 'pizza'}})
+      .expect({food: 'pizza'})
   });
 
   it('should allow the model property to be overridden', function () {
-    expectModel(
-      { food: 'string'},
-      {body: {food: 'pizza'}},
-      {food: 'pizza'},
-      "dataObj"
-    )
+    withModel({food: 'string'}, "dataObj")
+      .request({body: {food: 'pizza'}})
+      .expect({food: 'pizza'});
   });
 
   it('does not allow invalid enum values', function () {
-    expectError(
-      { food: {type: 'string', enum: ['pizza', 'hamburger', 'steak']}},
-      {body: {food: 'eggs'}}
-    )
+    withModel({food: {type: 'string', enum: ['pizza', 'hamburger', 'steak']}})
+      .request({body: {food: 'eggs'}})
+      .expectError();
   });
 
   it('converts names', function () {
-    expectModel(
-      { food: {type: 'string', name: "food_choice"}},
-      {body: {food_choice: 'pizza'}},
-      {food: 'pizza'}
-    )
+    withModel({food: {type: 'string', name: "food_choice"}})
+      .request({body: {food_choice: 'pizza'}})
+      .expect({food: 'pizza'})
   });
 
-  function expectError(definition, req) {
-    const middleware = model(definition);
-
-    let statusCode = -1;
-    const res = {
-      status: (code) => {
-        statusCode = code;
-        return {send: () => {}}
-      }
-    };
-
-    let nextCalls = 0;
-    middleware(req, res, () => nextCalls++);
-
-    assert.strictEqual(nextCalls, 0, `Next should NOT be called when validation fails`);
-    assert.strictEqual(statusCode, 400, "Request should have been rejected with status code 400");
-  }
-
-  function expectModel(definition, req, expectedModel, modelProp="model") {
+  /**
+   * Helper function that creates and runs the model middleware.
+   *
+   * @param definition A model definition
+   * @param modelProp The output property. defaults to 'model'
+   * @returns {{request: (function(*=): {expect: expect, expectError: expectError})}}
+   */
+  function withModel(definition, modelProp = "model") {
     const middleware = model(definition, modelProp);
+    return {
+      request: (req) => {
+        return {
+          expect: (expected) => {
+            let nextCalls = 0;
+            middleware(req, {}, () => nextCalls++);
 
-    let nextCalls = 0;
-    middleware(req, {}, () => nextCalls++);
+            assert.strictEqual(nextCalls, 1, `Next should have been called once, got ${nextCalls}`);
+            assert.deepStrictEqual(req[modelProp], expected, `Expected req.${modelProp} did not match actual`);
+          },
 
-    assert.strictEqual(nextCalls, 1, `Next should have been called once, got ${nextCalls}`);
-    assert.deepStrictEqual(req[modelProp], expectedModel, `Expected req.${modelProp} did not match actual`);
+          expectError: () => {
+            let statusCode = -1;
+            const res = {
+              status: (code) => {
+                statusCode = code;
+                return {
+                  send: () => {
+                  }
+                }
+              }
+            };
+
+            let nextCalls = 0;
+            middleware(req, res, () => nextCalls++);
+
+            assert.strictEqual(nextCalls, 0, `Next should NOT be called when validation fails`);
+            assert.strictEqual(statusCode, 400, "Request should have been rejected with status code 400");
+          }
+        }
+      }
+    }
   }
 });
